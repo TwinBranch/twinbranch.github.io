3383cb133a6a16e6f44998e4548b75bb9dcdd0ea
TwinBranch <mturnbull@Matthews-MacBook-Pro.local>
2016-11-02 13:27:55 -0400

    dklsd
    cmvnk;

---
+++ game.js
@@ -0,0 +1,17 @@
+var team = "Cowboys";
+var team = "Giants";
+var cPlayers = "Dak Prescott, Ezekiel Elliot";
+var gPlayers = "Eli Manning, Odell Beckham Jr.";
+
+
+if team = "Cowboys" {
+
+  document.write(cPlayers);
+
+}
+
+else {
+
+  document.write(gPlayers);
+
+}
