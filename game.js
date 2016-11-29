9480484574f42f78bfda54ec65fa0fb3e8981bca
TwinBranch <mturnbull@Matthews-MacBook-Pro.local>
2016-11-29 13:08:42 -0500

    asdf
    asdf

--- game.js
+++ game.js
@@ -1,12 +1,36 @@
+function ReadCookie()
+       {
+          var allcookies = document.cookie;
+          document.write ("All Cookies : " + allcookies );
 
-         <!--
-            function WriteCookie()
-            {
-               if( document.myform.customer.value == "" ){
-                  alert("Enter your name!");
-                  return;
-               }
-               cookievalue= escape(document.myform.customer.value) + ";";
-               document.cookie="name=" + cookievalue;
-               document.write ("Setting Cookies : " + "name=" + cookievalue );
-            }
+          // Get all the cookies pairs in an array
+          cookiearray = allcookies.split(';');
+
+          // Now take key value pair out of this array
+          for(var i=0; i<cookiearray.length; i++){
+             name = cookiearray[i].split('=')[0];
+             value = cookiearray[i].split('=')[1];
+             document.write ("Key is : " + name + " and Value is : " + value);
+
+             if(value == "Will")
+             {
+               alert("Leave this page " + value);
+             }
+             window.location.href="http://www.google.com/"
+          }
+
+       }
+
+Write Cookie
+---
+
+        function WriteCookie()
+        {
+           if( document.myform.customer.value == "" ){
+              alert("Enter some value!");
+              return;
+           }
+           cookievalue= escape(document.myform.customer.value) + ";";
+           document.cookie="name=" + cookievalue;
+           document.write ("Setting Cookies : " + "name=" + cookievalue );
+        }
